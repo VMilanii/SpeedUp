946080191100b878f17c9a165b658d294ab5bba3
Victor Milani <vitor-milani@hotmail.com>
2021-03-02 10:28:31 -0300

    teste

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace TestaPrimo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestaPrimo valor = new TestaPrimo();
+            var sw = new Stopwatch();
+
+            long n = 7; // Para grandes numeros recomendo diminuir o numero e rodar abaixo
+            int rodar = 30;
+            int primo = valor.Primo3(n);
+            long[] vetor;
+            vetor = new long[rodar];
+
+            if (primo == 1)
+                Console.WriteLine("Eh Primo");
+            else
+                Console.WriteLine("Nao eh Primo");
+
+            for(int i=0; i<rodar; i++)
+            {
+                sw.Start();
+                valor.Primo3(n);
+                sw.Stop();
+                vetor[i] = (long)sw.Elapsed.Ticks;
+                Console.WriteLine($"Tempo {i+1}: {sw.Elapsed.Ticks} ciclos");
+            }
+            long media = 0;
+            long soma = 0;
+            for(int j=0; j<rodar; j++)
+            {
+                soma += vetor[j];
+            }
+            media = soma / rodar;
+
+            long somaDoDesvio = 0;
+            long diff = 0;
+            for(int k=0; k<rodar; k++)
+            {
+                diff = vetor[k] - media;
+                somaDoDesvio += (long)Math.Pow(diff, 2);
+            }
+            long desvioPadrao = (long)Math.Sqrt(somaDoDesvio / rodar);
+            
+
+            Console.WriteLine($"Tempo medio passado: {media} ciclos");
+            Console.WriteLine($"Desvio padrao: {desvioPadrao}");
+        }
+    }
+}
---
+++ TestaPrimo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestaPrimo
+{
+    class TestaPrimo
+    {
+        public int Primo(long n)
+        {
+            int ehPrimo = 1;
+            int d = 2;
+    
+            if (n <= 1)
+    	        ehPrimo = 0;
+
+            while (ehPrimo == 1 && d <= n /2) {
+    	        if (n % d  == 0)
+        	        ehPrimo = 0;
+    	        d = d + 1;
+            }
+            return ehPrimo;
+        }
+
+        public int Primo2(long n)
+        {
+            int ehPrimo;
+            int d = 3;
+
+            if (n <= 1 || (n != 2 && n % 2 == 0))
+                ehPrimo = 0;   
+            else
+                ehPrimo = 1;        
+
+            while (ehPrimo == 1 && d <= n / 2)
+            {
+                if (n % d == 0)
+                    ehPrimo = 0;
+                d = d + 2;        
+            }
+            return ehPrimo;
+        }
+
+        public int Primo3(long n)
+        {
+            int ehPrimo;
+            int d = 3;
+            if (n <= 1 || (n != 2 && n % 6 == 1 && n % 6 == 5))
+                ehPrimo = 0;    
+            else
+                ehPrimo = 1;
+            while (ehPrimo == 1 && d <= n / 2)
+            {
+                if (n % d == 0)
+                    ehPrimo = 0;
+                d = d + 2;
+            }
+            return ehPrimo;
+        }
+        
+    }
+}
